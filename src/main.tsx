ebe3f898640e1d80f686260571f0bc063d5d0c75
RASHID-RGF <rashid@example.com>
2026-02-09 21:08:35 -0800

    final commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import App from "./App.tsx";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>,
 );
